de8df72304e53b3d23572e1048cda15c2df5b2a8
Ania <ania.goworek@gmail.com>
2024-04-01 21:17:06 +0200

    Save training and test sets into pickle files

--- transform_data.py
+++ transform_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import pickle
+import os
 
 # Import data
 with open('data.csv', 'r') as file:
@@ -72,7 +74,7 @@ train_size = int(0.75 * total_columns)
 train_indices_list = [indices[i:i+train_size] for i in range(0, total_columns, train_size)]
 test_indices_list = [np.setdiff1d(indices, train_indices) for train_indices in train_indices_list]
 
-# Create 30 training and test sets for y and X
+# create 30 training and test sets for y and X
 train_y_list = [y.iloc[:, train_indices.tolist()] for train_indices in train_indices_list]
 test_y_list = [y.iloc[:, test_indices.tolist()] for test_indices in test_indices_list]
 train_X_list = [X.iloc[:, train_indices.tolist()] for train_indices in train_indices_list]
@@ -80,3 +82,13 @@ test_X_list = [X.iloc[:, test_indices.tolist()] for test_indices in test_indices
 
 print(train_X_list[0])
 print(train_y_list[0])
+
+# save training and test sets into pickle files
+folder_path = "training_and_test_sets"
+os.makedirs(folder_path, exist_ok=True)
+datasets = [train_y_list, test_y_list, train_X_list, test_X_list]
+
+for name, dataset_list in zip(["train_y", "test_y", "train_X", "test_X"], datasets):
+    for i, dataset_df in enumerate(dataset_list):
+        with open(os.path.join(folder_path, f"{name}_{i+1}.pkl"), "wb") as f:
+            pickle.dump(dataset_df, f)
