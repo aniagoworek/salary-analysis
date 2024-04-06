744e43268861a703924267fb5b1d0ca1062a5ad4
Ania <ania.goworek@gmail.com>
2024-04-06 18:58:02 +0200

    Adapt data transformation to new data, create 30 train and test sets for 6 different dfs

--- transform_data.py
+++ transform_data.py
@@ -1,99 +1,36 @@
 import pandas as pd
 import numpy as np
-import pickle
-import os
+from sklearn.model_selection import train_test_split
 
 # Import data
-with open('data.csv', 'r') as file:
-    lines = file.readlines()
+df = pd.read_csv('data.csv', delimiter=';')
 
-# Data as list
-data = [line.strip().split(';') for line in lines]
-
-variables = data[0]
-dates_row = data[1][2:]
-dates = pd.to_datetime(dates_row, format='%Y')
-df = pd.DataFrame(data[1:], columns=variables)
-df_transposed = df.transpose()
+# Transform data
+df['Miejsce'] = df['Miejsce'].astype('category')
+df.drop(['Kod powiatu'], axis=1, inplace=True)
+df = df.select_dtypes(include=[np.number])
 # print(df)
-
-df_transposed.insert(0, 'Variables', df_transposed.index)
-df_transposed.reset_index(drop=True, inplace=True)
-df_transposed = df_transposed.iloc[1:] # delete unneccesary row
-df_transposed.reset_index(drop=True, inplace=True)
-# print(df_transposed) 
-
-# conversion of regions into indexes
-df_transposed.columns = df_transposed.iloc[0] #first row as the column header
-df_transposed = df_transposed.iloc[1:] # delete the first row that has become the column header
-# print(df_transposed)
-
-df_transposed.rename(columns={'Nazwa': 'Variable'}, inplace=True)
-df_transposed.rename(columns={'': 'Year'}, inplace=True)
-# print(df_transposed)
-
-# convert data to float
-def convert_to_numeric(value):
-    try:
-        return pd.to_numeric(value)
-    except ValueError:
-        return value
-
-
-df_numeric = df_transposed.applymap(convert_to_numeric)
-df_numeric['Variable'] = df_numeric['Variable'].astype('category')
-df_numeric['Year'] = pd.to_datetime(df_numeric['Year'], format='%Y')
-# print(df_numeric)
-# print("Data types:")
-# print(df_numeric.dtypes)
-
-# save places, years and variables
-places = df_numeric.index.unique(level=0)
-years = df_numeric['Year']
-variables = df_numeric['Variable']
-print(df_numeric)
-
-# add y
-y = df_numeric.loc[df_numeric.iloc[:, 0] == "wynagrodzenia"]
-# print(y)
-# print(y.drop('Year', axis=1).describe())
-
-# add X
-df_without_y = df_numeric[df_numeric['Variable'] != 'wynagrodzenia']
-X = df_without_y.sort_values(by='Variable')
-# print(X)
-
-# create random division of y and X into training and test sets
+# print(df.dtypes)
+df_2016_2017 = df.filter(regex='- 201[67]')
+df_2016_2018 = df.filter(regex='- 201[678]')
+df_2016_2019 = df.filter(regex='- 201[6789]')
+df_2016_2020 = df.filter(regex='- 201[67890]')
+df_2016_2021 = df.filter(regex='- 201[678901]')
+df_2016_2022 = df.filter(regex='- 201[6789012]')
+# print(df_2016_2017.head())
 np.random.seed(73)
-num_sets = 30 # number of training and test sets
+# Split data into training and testing sets
+training_sets = []
+testing_sets = []
 
-total_columns = X.shape[1] - 2  # number of columns in X without column of variables and dates
-train_size = int(0.75 * total_columns)
-
-train_indices_list = []
-test_indices_list = []
-
-for _ in range(num_sets):
-    train_indices = np.random.choice(total_columns, train_size, replace=True)
-    test_indices = np.setdiff1d(np.arange(total_columns), train_indices)
+for df_year in [df_2016_2017, df_2016_2018, df_2016_2019, df_2016_2020, df_2016_2021, df_2016_2022]:
+    X = df_year.drop(columns=[f'wynagrodzenia - {df_year.columns[-1][-4:]}'])
+    y = df_year[f'wynagrodzenia - {df_year.columns[-1][-4:]}']
     
-    train_indices_list.append(train_indices)
-    test_indices_list.append(test_indices)
+    for _ in range(30):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=73)
+        training_sets.append((X_train, y_train))
+        testing_sets.append((X_test, y_test))
 
-# create 30 training and test sets for y and X
-train_y_list = [y.iloc[:, train_indices] for train_indices in train_indices_list]
-test_y_list = [y.iloc[:, test_indices] for test_indices in test_indices_list]
-train_X_list = [X.iloc[:, train_indices] for train_indices in train_indices_list]
-test_X_list = [X.iloc[:, test_indices] for test_indices in test_indices_list]
-# print(train_X_list[9])
-# print(train_y_list[0])
-
-# save training and test sets into pickle files
-folder_path = "training_and_test_sets"
-os.makedirs(folder_path, exist_ok=True)
-datasets = [train_y_list, test_y_list, train_X_list, test_X_list]
-
-for name, dataset_list in zip(["train_y", "test_y", "train_X", "test_X"], datasets):
-    for i, dataset_df in enumerate(dataset_list):
-        with open(os.path.join(folder_path, f"{name}_{i+1}.pkl"), "wb") as f:
-            pickle.dump(dataset_df, f)
+print(testing_sets[29][0]) # pierwsza liczba oznacza numer zbioru, druga: 0 - X, 1 - y
+print(training_sets[60][0]) # pierwsza liczba: 0-29 = lata 2016-2017, 30-59 = lata 2016-2018, 60-89 = lata 2016-2019, 90-119 = lata 2016-2020, 120-149 = lata 2016-2021, 150-179 = lata 2016-2022
