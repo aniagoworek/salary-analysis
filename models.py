3953e55e23f6998641270cfe6617052f41281ebb
Ania <ania.goworek@gmail.com>
2024-04-11 11:46:41 +0200

    Fix the fuction to select the best parameters

--- models.py
+++ models.py
@@ -60,27 +60,25 @@ def tune_rf_hyperparameters(inversed_filled_training_sets, k, n):
 
 def find_optimal_hyperparameters(inversed_filled_training_sets, k, n):
     param_grid = {
-        'n_estimators': range(30, 301),
+        'n_estimators': range(30, 301, 10),
         'max_depth': [None, 5, 10, 15, 20],
         'min_samples_split': [2, 5, 10, 15, 20],
         'min_samples_leaf': [1, 2, 3, 4, 5],
-        'max_features': ['auto', 'sqrt', 'log2']
+        'max_features': ['1.0', 'sqrt', 'log2']
     }
-    models = GradientBoostingRegressor()
+    model = GradientBoostingRegressor()
     best_params = []
 
-    for model in models:
-        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=n-k)
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=n-k)
 
-        for i in range(k, n):
-            X_train = inversed_filled_training_sets[i][0]
-            y_train = inversed_filled_training_sets[i][1].values.ravel()
-            grid_search.fit(X_train, y_train)
+    X_train = inversed_filled_training_sets[k][0]
+    y_train = inversed_filled_training_sets[k][1].values.ravel()
+    grid_search.fit(X_train, y_train)
 
-        best_params.append(grid_search.best_params_)
+    best_params.append(grid_search.best_params_)
 
-        print("Best Parameters:")
-        print(grid_search.best_params_)
+    print("Best Parameters:")
+    print(grid_search.best_params_)
 
     return best_params
 
@@ -109,7 +107,7 @@ def model_rf(inversed_filled_training_sets, inversed_filled_testing_sets, k, n, 
     median_error = np.median(errors)
     print("\nrandom forest")
     print(f"Median Error: {median_error}%")
-    return model
+    return model, errors
 
 
 def model_boost(inversed_filled_training_sets, inversed_filled_testing_sets, k, n):
@@ -125,26 +123,26 @@ def model_boost(inversed_filled_training_sets, inversed_filled_testing_sets, k, 
         y_test = inversed_filled_testing_sets[i][1].values.ravel()
         predictions = model.predict(X_test)
 
-    feature_importances = []
-    for i in range(k, n):
-        X_test = inversed_filled_testing_sets[i][0]
-        feature_importances.append(model.feature_importances_)
-    median_importance = np.median(feature_importances, axis=0)
-    variable_names = inversed_filled_training_sets[k][0].columns
-    print("\nFeature Importances - boosting:")
-    sorted_importances = sorted(zip(variable_names, median_importance), key=lambda x: x[1], reverse=True)
-    for name, importance in sorted_importances:
-        print(f"{name}: {importance}")
+    # feature_importances = []
+    # for i in range(k, n):
+    #     X_test = inversed_filled_testing_sets[i][0]
+    #     feature_importances.append(model.feature_importances_)
+    # median_importance = np.median(feature_importances, axis=0)
+    # variable_names = inversed_filled_training_sets[k][0].columns
+    # print("\nFeature Importances - boosting:")
+    # sorted_importances = sorted(zip(variable_names, median_importance), key=lambda x: x[1], reverse=True)
+    # for name, importance in sorted_importances:
+    #     print(f"{name}: {importance}")
 
         # Feature Importances - boosting
-        plt.figure(figsize=(10, 6))
-        plt.bar(variable_names, median_importance)
-        plt.title('Feature Importances - Boosting')
-        plt.xlabel('Variable')
-        plt.ylabel('Importance')
-        plt.xticks(rotation=90)
-        plt.savefig(f'feature_importance_plot{i - k + 2}.svg', format='svg')
-        plt.show()
+        # plt.figure(figsize=(10, 6))
+        # plt.bar(variable_names, median_importance)
+        # plt.title('Feature Importances - Boosting')
+        # plt.xlabel('Variable')
+        # plt.ylabel('Importance')
+        # plt.xticks(rotation=90)
+        # plt.savefig(f'feature_importance_plot{i - k + 2}.svg', format='svg')
+        # plt.show()
 
     errors = []
     for i in range(k, n):
@@ -164,7 +162,7 @@ def model_boost(inversed_filled_training_sets, inversed_filled_testing_sets, k, 
     median_error = np.median(errors)
     print("\nboosting")
     print(f"Median Error: {median_error}%")
-    return model
+    return model, errors
 
 
 def model_bagging(inversed_filled_training_sets, inversed_filled_testing_sets, k, n, bootstrap, bootstrap_features, max_features, max_samples, n_estimators):
@@ -193,7 +191,7 @@ def model_bagging(inversed_filled_training_sets, inversed_filled_testing_sets, k
     print("\nbagging")
     print(f"Median Error: {median_error}%")
 
-    return model
+    return model, errors
 
 
 def model_cnn(standarized_filled_training_sets, standarized_filled_testing_sets, k , n):
@@ -228,6 +226,6 @@ def model_cnn(standarized_filled_training_sets, standarized_filled_testing_sets,
     print("\n cnn")
     print(f"Median Error: {median_error}%")
 
-    return model
+    return model, errors
 
 
